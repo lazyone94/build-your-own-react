d9648445852bd560ef3495b818da40481f369276
zhusifan <zhusifan.0419@bytedance.com>
2023-07-04 20:00:00 +0800

    doc: 添加文档

--- index.js
+++ index.js
@@ -2,26 +2,6 @@ import Didact from './Didact/index.js';
 
 const container = document.getElementById('root');
 
-// const element = Didact.createElement(
-//   'div',
-//   {
-//     id: 'foo',
-//   },
-//   Didact.createElement('a', null, 'bar'),
-//   Didact.createElement(
-//     'div',
-//     {
-//       id: 'foo-1',
-//       name: 'click',
-//       class: 'foo-1',
-//       onClick: () => {
-//         console.log('zsf');
-//       },
-//     },
-//     '123'
-//   )
-// );
-
 function App(props) {
   const [state, setSate] = Didact.useState(1);
   return Didact.createElement(
